5e43911c48189ce8372e3580f162ca112b12d90a
Colin Leong <122366389+cleong110@users.noreply.github.com>
2024-09-03 19:19:30 -0400

    CDL: updated scripts for overlaying/visualizing poses

--- videos_to_segments/overlay_pose_on_video.py
+++ videos_to_segments/overlay_pose_on_video.py
@@ -2,6 +2,12 @@ import argparse
 from pathlib import Path
 from pose_format import Pose
 from pose_format.pose_visualizer import PoseVisualizer
+from pose_format.utils.generic import pose_normalization_info
+from pose_format.utils.generic import pose_hide_legs
+import numpy as np
+# see also:
+# https://github.com/sign-language-processing/pose/blob/master/src/python/pose_format/bin/pose_visualizer.py
+
 # python overlay_pose_on_video.py -p "/home/vlab/Downloads/ASl_Citizen/ASL_Citizen/visualized_poses/002012924350435652-PAIR.pose" -v "/home/vlab/Downloads/ASl_Citizen/ASL_Citizen/visualized_poses/002012924350435652-PAIR.mp4"
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -13,12 +19,25 @@ if __name__ == "__main__":
     parser.add_argument(
         "-v", "--video_input", type=Path, required=True, help="Path to input video file"
     )
+
+    parser.add_argument("-n", "--normalize", default=False, action="store_true")
     parser.add_argument("-o", "--output", type=Path, help="output video path")
 
     args = parser.parse_args()
 
     with open(str(args.pose_input), "rb") as f:
         pose = Pose.read(f.read())
+
+    pose_hide_legs(pose)
+    if args.normalize:
+        print("normalizing pose")
+
+        pose = pose.normalize(pose_normalization_info(pose.header))
+        new_width = 500
+        shift = 1.25
+        shift_vec = np.full(shape=(pose.body.data.shape[-1]), fill_value=shift, dtype=np.float32)
+        pose.body.data = (pose.body.data + shift_vec) * new_width
+        pose.header.dimensions.height = pose.header.dimensions.width = int(new_width * shift * 2)
 
     v = PoseVisualizer(pose)
 
---
+++ videos_to_segments/overlay_videos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -o errexit 
+set -o pipefail
+set -o nounset
+
+dir_to_search=$1
+# TODO: activate correct env
+
+
+
+#find "$dir_to_search"/*.pose | parallel -j1 visualize_pose -i "{}" -o "{.}".pose.mp4
+#find "$dir_to_search"/*.pose | parallel -j1 visualize_pose --normalize -i "{}" -o "{.}".pose.normalized.mp4
+find "$dir_to_search"/*.pose | parallel -j1 python overlay_pose_on_video.py -v "{.}".mp4 -p "{}"
+find "$dir_to_search"/*.pose | parallel -j1 python overlay_pose_on_video.py -v "{.}".mp4 -p "{}" --normalize -o "{.}".overlay.normalized.mp4
