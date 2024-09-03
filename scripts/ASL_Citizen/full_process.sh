918211cb9e3d3de1de41da7b055928b4711851c1
Colin Leong <122366389+cleong110@users.noreply.github.com>
2024-09-03 11:58:34 -0400

    CDL: parameterizing a script

--- scripts/ASL_Citizen/full_process.sh
+++ scripts/ASL_Citizen/full_process.sh
@@ -4,15 +4,21 @@ set -o pipefail
 set -o nounset
 
 
-mkdir -p ./posed_videos/
-./0_parallel_video_to_pose.sh ./videos
-./1a_move_finished_poses.sh ./videos/ ./posed_videos/
-./1b_remove_the_word_seed_from_filenames.sh ./posed_videos/ 
-./1c_remove_spaces_with_underscores_in_filenames.sh ./posed_videos/
-./2_find_lexicon_from_filenames.sh posed_videos > posed_videos_lexicon_list.txt
-./3_pick_n_random_words_from_lexicon.sh posed_videos_lexicon_list.txt 500 > 500_random_words.txt
-./4_count_videos_per_word_in_list.sh 500_random_words.txt posed_videos > 500_random_words_sorted_by_counts.txt
+desired_lexicon_size=500
+top_n=400
+count_per_word=15
+src_dir="./videos"
+out_dir="./posed_videos"
 
-# optional, makes it neater because I had at least 10 per class
-tail -n 400 500_random_words_sorted_by_counts.txt |cut -d ":" -f1 > 400_most_frequent_from_500_random_words.txt
-./5_make_subset_from_lexicon.sh 400_most_frequent_from_500_random_words.txt 10 ./posed_videos ./asl_citizen_400_words_10_examples_each
+mkdir -p "$out_dir"/
+./0_parallel_video_to_pose.sh "$src_dir"
+./1a_move_finished_poses.sh "$src_dir"/ "$out_dir"/
+./1b_remove_the_word_seed_from_filenames.sh "$out_dir"/ 
+./1c_remove_spaces_with_underscores_in_filenames.sh "$out_dir"/
+./2_find_lexicon_from_filenames.sh "$out_dir" > full_lexicon_from_filenames.txt
+./3_pick_n_random_words_from_lexicon.sh full_lexicon_from_filenames.txt "$desired_lexicon_size" > "$desired_lexicon_size"_random_words.txt
+./4_count_videos_per_word_in_list.sh "$desired_lexicon_size"_random_words.txt "$out_dir" > "$desired_lexicon_size"_random_words_sorted_by_counts.txt
+
+# take the top N with the most examples. optional, makes it neater because I had at least "$count_per_word" per class
+tail -n "$top_n" "$desired_lexicon_size"_random_words_sorted_by_counts.txt |cut -d ":" -f1 > "$top_n"_most_frequent_from_"$desired_lexicon_size"_random_words.txt
+./5_make_subset_from_lexicon.sh "$top_n"_most_frequent_from_"$desired_lexicon_size"_random_words.txt "$count_per_word" "$out_dir" ./asl_citizen_"$top_n"_words_"$count_per_word"_examples_each
