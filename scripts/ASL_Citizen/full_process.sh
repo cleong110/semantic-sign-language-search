b3dca8a52733343a32e5c8e2ea1fccce52f02b6a
Colin Leong <122366389+cleong110@users.noreply.github.com>
2024-09-03 11:12:49 -0400

    CDL: typo

--- scripts/ASL_Citizen/full_process.sh
+++ scripts/ASL_Citizen/full_process.sh
@@ -4,10 +4,11 @@ set -o pipefail
 set -o nounset
 
 
+mkdir -p ./posed_videos/
 ./0_parallel_video_to_pose.sh ./videos
 ./1a_move_finished_poses.sh ./videos/ ./posed_videos/
 ./1b_remove_the_word_seed_from_filenames.sh ./posed_videos/ 
-./1c_remove_the_word_seed_from_filenames.sh ./posed_videos/
+./1c_remove_spaces_with_underscores_in_filenames.sh ./posed_videos/
 ./2_find_lexicon_from_filenames.sh posed_videos > posed_videos_lexicon_list.txt
 ./3_pick_n_random_words_from_lexicon.sh posed_videos_lexicon_list.txt 500 > 500_random_words.txt
 ./4_count_videos_per_word_in_list.sh 500_random_words.txt posed_videos > 500_random_words_sorted_by_counts.txt
