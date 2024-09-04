23805c8ab8d182f9a5d469b3a833271d41db9510
Colin Leong <122366389+cleong110@users.noreply.github.com>
2024-09-04 17:50:45 -0400

    CDL: updated script to create the db, etc

--- setup_db/embedding_db.py
+++ setup_db/embedding_db.py
@@ -7,14 +7,15 @@ db_name = "mydb"
 
 # Create an object that handles connections and queries
 db = PostgresqlDatabase(db_name)
+table_model_names = []
 
 class BaseModel(Model):
     class Meta:
         database = db  # this model uses the database specified with "db_name" above
 
-class Sign(BaseModel):
-    gloss=CharField(unique=True)
-    language_code=CharField()
+# class Sign(BaseModel):
+#     gloss=CharField(unique=True)
+#     language_code=CharField()
 
 class Embedding(BaseModel):
     input_modality=CharField()
@@ -47,15 +48,22 @@ def drop_and_recreate(model_names):
     db.drop_tables(model_names, safe=True)
     db.create_tables(model_names)
 
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="TODO", description="setup embedding search", epilog="TODO"
     )
     parser.add_argument("--recreate", help="drop and recreate database", action="store_true")
+    # parser.add_argument("--db_name", help="database to connect to", default=None)
     args = parser.parse_args()
-    model_names=[SignVideo, Sign, Pose, Embedding]
+
+    table_model_names=[SignVideo, 
+                #  Sign, 
+                 Pose, 
+                 Embedding]
 
 
     if args.recreate:
         print("dropping and recreating database")
-        drop_and_recreate(model_names)
+        drop_and_recreate(table_model_names)
