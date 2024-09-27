26ba5aae8681c31cca4987c200656d01f0ed4754
Colin Leong <122366389+cleong110@users.noreply.github.com>
2024-09-27 15:25:27 -0400

    CDL: incorporate pose visualization fix from https://github.com/sign-language-processing/pose/issues/114

--- videos_to_segments/overlay_pose_on_video.py
+++ videos_to_segments/overlay_pose_on_video.py
@@ -1,5 +1,6 @@
 import argparse
 from pathlib import Path
+from typing import List
 from pose_format import Pose
 from pose_format.pose_visualizer import PoseVisualizer
 from pose_format.utils.generic import pose_normalization_info
@@ -7,6 +8,18 @@ from pose_format.utils.generic import pose_hide_legs
 import numpy as np
 # see also:
 # https://github.com/sign-language-processing/pose/blob/master/src/python/pose_format/bin/pose_visualizer.py
+
+
+def remove_world_landmarks(pose: Pose):
+    remove_specified_landmarks(pose, "POSE_WORLD_LANDMARKS")
+    
+def remove_specified_landmarks(pose: Pose, landmark_names:List[str]):
+    if isinstance(landmark_names, str):
+        landmark_names = [landmark_names]
+    components_without_specified_names = [c.name for c in pose.header.components if c.name not in landmark_names]
+    new_pose = pose.get_components(components_without_specified_names)
+    pose.body=new_pose.body
+    pose.header=new_pose.header
 
 # python overlay_pose_on_video.py -p "/home/vlab/Downloads/ASl_Citizen/ASL_Citizen/visualized_poses/002012924350435652-PAIR.pose" -v "/home/vlab/Downloads/ASl_Citizen/ASL_Citizen/visualized_poses/002012924350435652-PAIR.mp4"
 if __name__ == "__main__":
@@ -19,9 +32,12 @@ if __name__ == "__main__":
     parser.add_argument(
         "-v", "--video_input", type=Path, required=True, help="Path to input video file"
     )
+    parser.add_argument("-o", "--output", type=Path, help="Output video path")
 
     parser.add_argument("-n", "--normalize", default=False, action="store_true")
-    parser.add_argument("-o", "--output", type=Path, help="output video path")
+    
+    parser.add_argument("--keep_world_landmarks", action="store_true", help="Keep the world landmarks, even though it makes for weird visual bugs as described in https://github.com/sign-language-processing/pose/issues/114") # store_true is false by default
+    
 
     args = parser.parse_args()
 
@@ -29,6 +45,16 @@ if __name__ == "__main__":
         pose = Pose.read(f.read())
 
     pose_hide_legs(pose)
+    
+    if args.keep_world_landmarks:
+        print("Not removing world landmarks")
+    else:
+        print("Removing world landmarks")
+        print([c.name for c in pose.header.components])        
+        remove_world_landmarks(pose)
+        print("removed world landmarks")
+        print([c.name for c in pose.header.components])        
+
     if args.normalize:
         print("normalizing pose")
 
