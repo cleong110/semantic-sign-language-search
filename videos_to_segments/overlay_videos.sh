ddf4b7becb94ba2bda9e1e4ac5d98891f1ba2368
Colin Leong <122366389+cleong110@users.noreply.github.com>
2024-09-27 15:33:32 -0400

    CDL; update overlay videos with poses script a little

--- videos_to_segments/overlay_videos.sh
+++ videos_to_segments/overlay_videos.sh
@@ -3,12 +3,12 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-dir_to_search=$1
+dir_to_search="$1"
 # TODO: activate correct env, e.g. signsegment
 
 
 
-find "$dir_to_search"/*.pose | parallel -j1 visualize_pose -i "{}" -o "{.}".pose.mp4
-find "$dir_to_search"/*.pose | parallel -j1 visualize_pose --normalize -i "{}" -o "{.}".pose.normalized.mp4
-find "$dir_to_search"/*.pose | parallel -j1 python overlay_pose_on_video.py -v "{.}".mp4 -p "{}"
-find "$dir_to_search"/*.pose | parallel -j1 python overlay_pose_on_video.py -v "{.}".mp4 -p "{}" --normalize -o "{.}".overlay.normalized.mp4
+find "$dir_to_search"/*.pose | parallel --progress --eta -j1 visualize_pose -i "{}" -o "{.}".pose.mp4
+find "$dir_to_search"/*.pose | parallel --progress --eta -j1 visualize_pose --normalize -i "{}" -o "{.}".pose.normalized.mp4
+find "$dir_to_search"/*.pose | parallel --progress --eta -j1 python overlay_pose_on_video.py -v "{.}".mp4 -p "{}"
+# find "$dir_to_search"/*.pose | parallel -j1 python overlay_pose_on_video.py -v "{.}".mp4 -p "{}" --normalize -o "{.}".overlay.normalized.mp4
